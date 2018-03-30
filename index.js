80d0a806c1e68e05082939090c03a2d6726b3dd8
Aleksey <aleks.dev@gmail.com>
2018-03-30 12:28:37 +0300

    expose hideMessage()

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import MessageBar from './MessageBar';
 import messageManager from './messageManager';
 
 const showMessage = messageManager.showMessage.bind(messageManager);
+const hideMessage = messageManager.hideMessage.bind(messageManager);
 
 export {
   MessageBar,
   showMessage,
+  hideMessage,
 };
